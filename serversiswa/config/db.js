5591fa5ae323d935c12d0232ecf8953a744aee4e
mutapakkir <151417931+mutapakkir@users.noreply.github.com>
2024-12-12 06:12:08 -0800

    Add files via upload

--- serversiswa/config/db.js
+++ serversiswa/config/db.js
@@ -1,1 +1,14 @@
+const mySql = require('mysql2')
+require('dotenv').config()
 
+const pool = mySql.createPool({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password :process.env.DB_PASS,
+    database :process.env.DB_NAME,
+    port : process.env.DB_PORT
+
+})
+const poolPromise = pool.promise()
+
+module.exports = poolPromise
