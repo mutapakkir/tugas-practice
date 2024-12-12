9f851d3bce5d615091ac17602d486797bb325d6a
mutapakkir <151417931+mutapakkir@users.noreply.github.com>
2024-12-12 06:28:32 -0800

    Add files via upload

--- serversiswa/app.js
+++ serversiswa/app.js
@@ -1,1 +1,20 @@
 
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+require('dotenv').config()
+
+const studentRoutes = require('./routes/user-routers');
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded)()
+app.use('/api', studentRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,() => {
+    console.log(`Server run at http://localhost:${PORT}`);
+    
+})
